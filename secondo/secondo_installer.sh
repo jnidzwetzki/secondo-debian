17a9636214079616c06437f7dd293a06cd3e4a16
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-05-05 22:28:08 +0200

    Updated secondo build

---
+++ secondo/secondo_installer.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "================================="
+echo "Welcome to the SECONDO installer"
+echo "================================="
+
+if [ -f ~/.secondorc ]; then
+    echo "Error: Found an old secondo configuration '~/.secondorc'"
+    echo "To reconfigure SECONDO, please delete the file first"
+    exit -1
+fi
+
+databasedir=~/secondo-databases
+echo -n "Specify the directory for your databases [$databasedir]: "
+read databasedir2
+
+if [ -n "$databasedir2" ]; then
+    databasedir=$databasedir2
+fi
+
+mkdir -p $databasedir
+
+cp /opt/secondo/etc/SecondoConfig.ini ~/
+sed -i "s|SecondoHome=.*|SecondoHome=$databasedir|" ~/SecondoConfig.ini
+
+# Determine plattform
+architecture=$(getconf LONG_BIT)
+
+if [ $architecture -eq 64 ]; then
+cat <<-EOF > ~/.secondorc
+export SECONDO_BUILD_DIR=/opt/secondo
+export SECONDO_PLATFORM=linux64
+export SECONDO_CONFIG=~/SecondoConfig.ini
+export SWI_HOME_DIR=/usr/lib/swipl-6.6.5
+export PL_LIB_DIR=\$SWI_HOME_DIR/lib/x86_64-linux/
+export PL_DLL_DIR=\$SWI_HOME_DIR/lib/x86_64-linux/
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$SWI_HOME_DIR/lib:\$PL_LIB_DIR
+export PATH=\$PATH:\$SECONDO_BUILD_DIR/bin
+EOF
+else 
+export SECONDO_BUILD_DIR=/opt/secondo
+export SECONDO_PLATFORM=linux
+export SECONDO_CONFIG=~/SecondoConfig.ini
+export SWI_HOME_DIR=/usr/lib/swipl-6.6.5
+export PL_LIB_DIR=\$SWI_HOME_DIR/lib/i686-linux/
+export PL_DLL_DIR=\$SWI_HOME_DIR/lib/i686-linux/
+export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:\$SWI_HOME_DIR/lib:\$PL_LIB_DIR
+export PATH=\$PATH:\$SECONDO_BUILD_DIR/bin
+fi
+
+if [ $(grep secondorc ~/.bashrc | wc -l) -eq 0 ]; then
+   sed -i '1s|^|source ~/.secondorc\n|' ~/.bashrc
+fi
+
+echo ""
+echo "Installation complete."
+echo ""
+echo "Your databases will be stored in $databasedir"
+echo ""
+echo "Please re-login or execute 'source ~/.secondorc'"
+echo ""
+echo "Afterwards, you can start SECONDO by executing 'SecondoTTYBDB'"
+echo ""
+echo ""
+
