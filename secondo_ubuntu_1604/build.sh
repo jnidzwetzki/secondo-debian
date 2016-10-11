6e9024791029d11caab6e9223408ffe8ea16396b
Jan Nidzwetzki <jnidzwetzki@gmx.de>
2016-10-11 06:53:24 +0000

    Added additional algebras

--- secondo_ubuntu_1604/build.sh
+++ secondo_ubuntu_1604/build.sh
@@ -5,7 +5,7 @@
 #####################################################################
 
 # Install dependencies
-apt-get install flex bison gcc g++ libdb5.3 libdb5.3-dev libdb5.3++ libdb5.3++-dev db5.3-util libjpeg62 libjpeg62-dev libgsl0-dev libreadline-dev librecode-dev libgmp-dev libncurses-dev libxml2-dev libboost-all-dev build-essential debhelper fakeroot openjdk-8-jdk libxml2 libxml2-dev wget swi-prolog-nox swi-prolog-java swi-prolog
+apt-get install flex bison gcc g++ libdb5.3 libdb5.3-dev libdb5.3++ libdb5.3++-dev db5.3-util libjpeg62 libjpeg62-dev libgsl0-dev libreadline-dev librecode-dev libgmp-dev libncurses-dev libxml2-dev libboost-all-dev build-essential debhelper fakeroot openjdk-8-jdk libxml2 libxml2-dev wget swi-prolog-nox swi-prolog-java swi-prolog libquadmath0 libgmp-dev libgmp10 libboost-thread-dev libboost-thread1.58.0 libboost-system-dev libboost-system1.58.0
 
 VER=v400
 
@@ -30,6 +30,25 @@ sed -i 's|#ALGEBRA_INCLUDE_DIRS += /usr/include/libxml2|ALGEBRA_INCLUDE_DIRS += 
 sed -i 's|#ALGEBRA_DIRS += MP3|ALGEBRA_DIRS += MP3|g' secondo/makefile.algebras
 sed -i 's|#ALGEBRAS     += MP3Algebra|ALGEBRAS     += MP3Algebra|g' secondo/makefile.algebras
 
+sed -i 's|#ALGEBRA_DIRS += OptAux|ALGEBRA_DIRS += OptAux|g' secondo/makefile.algebras
+sed -i 's|#ALGEBRAS     += OptAuxAlgebra|ALGEBRAS     += OptAuxAlgebra|g' secondo/makefile.algebras
+
+#sed -i 's|#ALGEBRAS     += TinAlgebra|ALGEBRAS     += TinAlgebra|g' secondo/makefile.algebras
+#sed -i 's|#ALGEBRA_DEPS += quadmath gmp|ALGEBRA_DEPS += quadmath gmp|g' secondo/makefile.algebras
+
+sed -i 's|#ALGEBRA_DIRS += Distributed2|ALGEBRA_DIRS += Distributed2|g' secondo/makefile.algebras
+sed -i 's|#ALGEBRAS += Distributed2Algebra|ALGEBRAS += Distributed2Algebra|g' secondo/makefile.algebras
+sed -i 's|#DEFAULTCCFLAGS += -pthread -DTHREAD_SAFE|DEFAULTCCFLAGS += -pthread -DTHREAD_SAFE|g' secondo/makefile.algebras
+sed -i 's|#CCFLAGS += -pthread -DTHREAD_SAFE|CCFLAGS += -pthread -DTHREAD_SAFE|g' secondo/makefile.algebras
+sed -i 's|#COMMON_LD_FLAGS += -lboost_thread -lboost_system|COMMON_LD_FLAGS += -lboost_thread -lboost_system|g' secondo/makefile.algebras
+
+sed -i 's|#ALGEBRA_DIRS += RobustGeometry|ALGEBRA_DIRS += RobustGeometry|g' secondo/makefile.algebras
+sed -i 's|#ALGEBRAS   += RobustGeometryAlgebra|ALGEBRAS   += RobustGeometryAlgebra|g' secondo/makefile.algebras
+
+sed -i 's|#ALGEBRA_DIRS += MONTree|ALGEBRA_DIRS += MONTree|g' secondo/makefile.algebras
+sed -i 's|#ALGEBRAS     += MONTreeAlgebra|ALGEBRAS     += MONTreeAlgebra|g' secondo/makefile.algebras
+
+sed -i 's|# DEFAULTCCFLAGS += -DSM_FILE_ID|DEFAULTCCFLAGS += -DSM_FILE_ID|g' secondo/makefile.env
 cd secondo
 
 # Link our debian files to the package
